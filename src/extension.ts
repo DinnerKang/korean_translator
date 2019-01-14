fc88e41a1b3bd4933fd85df96670c91b257631e5
DinnerKang <jenhyuk0318@gmail.com>
2019-01-14 09:55:34 +0900

    update translate msg

--- src/extension.ts
+++ src/extension.ts
@@ -31,7 +31,7 @@ export function activate(context: ExtensionContext) {
         }
         console.log('text:', text);
 
-        
+
         translationText(editor);
 
 
@@ -56,17 +56,29 @@ export function activate(context: ExtensionContext) {
                         if (res.status == 200) {
                             console.log('성공');
                             return res.json();
-                        } else {
+                        } else if (res.status == 503) {
                             console.log('실패');
-                            vswindow.showInformationMessage('일일허용량을 넘겼습니다. 아마.. 수정하겠습니다.');
-                        }
+                            vswindow.showInformationMessage('카카오에 문제가 있습니다.');
+                        } 
                     }
                 ).then(resJson => {
                     console.log(resJson);
+                    if (resJson.translated_text[0] == '') {
+                        console.log('번역 불가');
+                        resJson.translated_text[0] = ['번역이 불가능합니다 ㅠ'];
+                    }
+                    console.log(resJson.translated_text[0]);
+
                     editor.edit((edit: any) => edit.replace(selection_range, String(resJson.translated_text[0])));
+
                     return resJson.translated_text[0];
                 })
-                .catch(err => console.log(err));
+                .catch(err => {
+                    if(err.code === -10){
+                        vswindow.showInformationMessage('API 허용량 초과입니다.');
+                    }
+                    
+                });
         }
 
     });
