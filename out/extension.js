170ab59f0453dc20833d57cfacd1452aab5fb10d
DinnerKang <jenhyuk0318@gmail.com>
2019-05-06 12:44:32 +0900

    2차 리펙토링

--- out/extension.js
+++ out/extension.js
@@ -21,78 +21,80 @@ var ref = db.ref('translate');
 var successRef = ref.child('success');
 var errorRef = ref.child('error');
 function activate(context) {
-    var date = new Date();
-    var year = String(date.getFullYear());
-    var month = String(date.getMonth() + 1);
-    var day = String(date.getDate());
-    var hour = String(date.getHours());
-    var minutes = String(date.getMinutes());
-    if (Number(month) < 10)
-        month = '0' + month;
-    if (Number(day) < 10)
-        day = '0' + day;
-    if (Number(hour) < 10)
-        hour = '0' + hour;
-    if (Number(minutes) < 10)
-        minutes = '0' + minutes;
-    // 시간
-    var time = year + month + day + hour + minutes;
     let disposable = vscode_1.commands.registerCommand('extension.translateKorean', () => {
+        function getNowTime() {
+            const date = new Date();
+            let year = String(date.getFullYear());
+            let month = String(date.getMonth() + 1);
+            let day = String(date.getDate());
+            let hour = String(date.getHours());
+            let minutes = String(date.getMinutes());
+            if (Number(month) < 10)
+                month = '0' + month;
+            if (Number(day) < 10)
+                day = '0' + day;
+            if (Number(hour) < 10)
+                hour = '0' + hour;
+            if (Number(minutes) < 10)
+                minutes = '0' + minutes;
+            const nowTime = year + month + day + hour + minutes;
+            return nowTime;
+        }
         let editor = vscode_1.window.activeTextEditor;
         if (!editor)
             return vscode_1.window.showInformationMessage('선택된 Text가 없음.');
         const selections = editor.selections[0];
-        const selection_range = new vscode_1.Range(selections.start, selections.end);
-        const text = editor.document.getText(selection_range);
-        const papago_header = {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'X-Naver-Client-Id': 'gBQ1QI8_eElsuFeCu8TC',
-            'X-Naver-Client-Secret': 'UPHmuQo2zi'
-        };
-        const translation_headers = {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'X-Naver-Client-Id': 'qtGsWHRHuDhH5fnL4vv_',
-            'X-Naver-Client-Secret': '4CNtCk1s5p'
-        };
+        const selectionRange = new vscode_1.Range(selections.start, selections.end);
+        const text = editor.document.getText(selectionRange);
         // papago 언어감지 API
-        function translator(text) {
+        function translateWords(text) {
             return __awaiter(this, void 0, void 0, function* () {
                 console.log('언어 감지할 Text', text);
+                const papagoHeader = {
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'X-Naver-Client-Id': 'gBQ1QI8_eElsuFeCu8TC',
+                    'X-Naver-Client-Secret': 'UPHmuQo2zi'
+                };
+                const translationHeaders = {
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'X-Naver-Client-Id': 'qtGsWHRHuDhH5fnL4vv_',
+                    'X-Naver-Client-Secret': '4CNtCk1s5p'
+                };
                 let target = 'en';
                 try {
-                    let language = yield fetch(`https://openapi.naver.com/v1/papago/detectLangs`, {
+                    const language = yield fetch(`https://openapi.naver.com/v1/papago/detectLangs`, {
                         method: 'POST',
-                        headers: papago_header,
+                        headers: papagoHeader,
                         body: `query=${text}`
-                    })
-                        .then(res => res.json());
-                    if (language.langCode == 'en')
+                    });
+                    const languageResult = yield language.json();
+                    if (languageResult.langCode === 'en')
                         target = 'ko';
-                    let translate = yield fetch(`https://openapi.naver.com/v1/papago/n2mt`, {
+                    const translate = yield fetch(`https://openapi.naver.com/v1/papago/n2mt`, {
                         method: 'POST',
-                        headers: translation_headers,
-                        body: `source=${language.langCode}&target=${target}&text=${text}`
-                    })
-                        .then(res => res.json());
-                    yield editor.edit((edit) => edit.replace(selection_range, String(translate.message.result.translatedText)));
+                        headers: translationHeaders,
+                        body: `source=${languageResult.langCode}&target=${target}&text=${text}`
+                    });
+                    const translateResult = yield translate.json();
+                    yield editor.edit((edited) => edited.replace(selectionRange, String(translateResult.message.result.translatedText)));
                     yield successRef.push({
-                        'Source': language.langCode,
+                        'Source': languageResult.langCode,
                         'Text': text,
-                        'Translation': translate.message.result.translatedText,
-                        'Time': time
+                        'Translation': translateResult.message.result.translatedText,
+                        'Time': getNowTime()
                     });
                 }
                 catch (err) {
                     errorRef.push({
                         'Text': text,
                         'Error': err,
-                        'Time': time
+                        'Time': getNowTime()
                     });
                     vscode_1.window.showInformationMessage(err);
                 }
             });
         }
-        translator(text);
+        translateWords(text);
     });
     context.subscriptions.push(disposable);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import {
-    window as vswindow,
+    window as vswindow, TextEditorEdit,
     commands,
     ExtensionContext,
     Range,
@@ -20,83 +20,84 @@ var errorRef = ref.child('error');
 
 
 export function activate(context: ExtensionContext) {
-    var date = new Date();
-
-    var year = String(date.getFullYear());
-    var month = String(date.getMonth() + 1);
-    var day = String(date.getDate());
-    var hour = String(date.getHours());
-    var minutes = String(date.getMinutes());
-
-    if (Number(month) < 10) month = '0' + month;
-    if (Number(day) < 10) day = '0' + day;
-    if (Number(hour) < 10) hour = '0' + hour;
-    if (Number(minutes) < 10) minutes = '0' + minutes;
-
-    // 시간
-    var time = year + month + day + hour + minutes;
-
     let disposable = commands.registerCommand('extension.translateKorean', () => {
+        
+        function getNowTime(){
+            const date = new Date();
+
+            let year = String(date.getFullYear());
+            let month = String(date.getMonth() + 1);
+            let day = String(date.getDate());
+            let hour = String(date.getHours());
+            let minutes = String(date.getMinutes());
+
+            if (Number(month) < 10) month = '0' + month;
+            if (Number(day) < 10) day = '0' + day;
+            if (Number(hour) < 10) hour = '0' + hour;
+            if (Number(minutes) < 10) minutes = '0' + minutes;
+            const nowTime = year + month + day + hour + minutes;
+
+            return nowTime;
+        }
 
         let editor = vswindow.activeTextEditor;
-
         if (!editor) return vswindow.showInformationMessage('선택된 Text가 없음.');
-
         const selections = editor.selections[0];
+        const selectionRange = new Range(selections.start, selections.end);
+        const text = editor.document.getText(selectionRange);
 
-        const selection_range = new Range(selections.start, selections.end);
-        const text = editor.document.getText(selection_range);
-
-        const papago_header = {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'X-Naver-Client-Id': 'gBQ1QI8_eElsuFeCu8TC',
-            'X-Naver-Client-Secret': 'UPHmuQo2zi'
-        };
-        const translation_headers = {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'X-Naver-Client-Id': 'qtGsWHRHuDhH5fnL4vv_',
-            'X-Naver-Client-Secret': '4CNtCk1s5p'
-        };
-
+        
         // papago 언어감지 API
-        async function translator(text: any) {
+        async function translateWords(text: String) {
             console.log('언어 감지할 Text', text);
+
+            const papagoHeader = {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'X-Naver-Client-Id': 'gBQ1QI8_eElsuFeCu8TC',
+                'X-Naver-Client-Secret': 'UPHmuQo2zi'
+            };
+            const translationHeaders = {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'X-Naver-Client-Id': 'qtGsWHRHuDhH5fnL4vv_',
+                'X-Naver-Client-Secret': '4CNtCk1s5p'
+            };
 
             let target = 'en';
             try{
-                let language = await fetch(`https://openapi.naver.com/v1/papago/detectLangs`,{
+                const language = await fetch(`https://openapi.naver.com/v1/papago/detectLangs`,{
                                             method: 'POST',
-                                            headers: papago_header,
-                                            body: `query=${text}`})
-                                            .then(res => res.json());
+                                            headers: papagoHeader,
+                                            body: `query=${text}`});
+                const languageResult = await language.json();
 
-                if(language.langCode == 'en') target = 'ko';
+                if(languageResult.langCode === 'en') target = 'ko';
                 
-                let translate = await fetch(`https://openapi.naver.com/v1/papago/n2mt`, {
+                const translate = await fetch(`https://openapi.naver.com/v1/papago/n2mt`, {
                                             method: 'POST',
-                                            headers: translation_headers,
-                                            body: `source=${language.langCode}&target=${target}&text=${text}`})
-                                            .then(res=> res.json());
-                
-                await editor!.edit((edit: any) => edit.replace(selection_range, String(translate.message.result.translatedText)));
+                                            headers: translationHeaders,
+                                            body: `source=${languageResult.langCode}&target=${target}&text=${text}`});
+                const translateResult = await translate.json();
+
+                await editor!.edit((edited: TextEditorEdit) => 
+                    edited.replace(selectionRange, String(translateResult.message.result.translatedText)));
+
                 await successRef.push({
-                        'Source': language.langCode,
+                        'Source': languageResult.langCode,
                         'Text': text,
-                        'Translation': translate.message.result.translatedText,
-                        'Time': time
+                        'Translation': translateResult.message.result.translatedText,
+                        'Time': getNowTime()
                 });
             }catch(err){
                 errorRef.push({
                     'Text': text,
                     'Error': err,
-                    'Time': time
+                    'Time': getNowTime()
                 });
                 vswindow.showInformationMessage(err);
             }
         }
-        translator(text);
+        translateWords(text);
     });
-
     context.subscriptions.push(disposable);
 }
 
